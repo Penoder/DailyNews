b70738c4f2d1f4695d9241dc07ab4e91290757a6
Penoder <Rocking7189@163.com>
2018-04-25 22:18:52 +0800

    Initial commit

--- app/src/main/java/com/penoder/dailynews/ui/MainActivity.java
+++ app/src/main/java/com/penoder/dailynews/ui/MainActivity.java
@@ -2,16 +2,18 @@ package com.penoder.dailynews.ui;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.databinding.ObservableField;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.app.Fragment;
 import android.view.Gravity;
 import android.view.ViewGroup;
 
 import com.penoder.dailynews.R;
 import com.penoder.dailynews.adapter.CommonFragmentAdapter;
 import com.penoder.dailynews.databinding.ActivityMainBinding;
+import com.penoder.dailynews.ui.basic.BaseFragment;
 import com.penoder.dailynews.ui.basic.BaseFragmentActivity;
+import com.penoder.dailynews.ui.bookshelf.ShelfFragment;
 import com.penoder.mylibrary.mvvm.command.ReplyCommand;
 import com.penoder.mylibrary.utils.ScreenUtils;
 import com.penoder.mylibrary.utils.ToastUtil;
@@ -27,9 +29,19 @@ public class MainActivity extends BaseFragmentActivity {
 
     private ActivityMainBinding mainBinding;
 
+    /**
+     * 用户名
+     */
+    public ObservableField<String> userName = new ObservableField<>();
+
+    /**
+     * 个人签名
+     */
+    public ObservableField<String> personSign = new ObservableField<>();
+
     private Context mContext;
 
-    private Fragment fragment = new Fragment();
+    private BaseFragment shelfFragment;
 
     private static Boolean isExit = false;
 
@@ -46,7 +58,9 @@ public class MainActivity extends BaseFragmentActivity {
     }
 
     private void initFragment() {
-        mainBinding.pagerMain.setAdapter(new CommonFragmentAdapter(getSupportFragmentManager(), fragment));
+        shelfFragment = new ShelfFragment();
+
+        mainBinding.pagerMain.setAdapter(new CommonFragmentAdapter(getSupportFragmentManager(), shelfFragment));
         mainBinding.pagerMain.setOffscreenPageLimit(1);
         mainBinding.pagerMain.setCurrentItem(0);
     }
@@ -57,16 +71,37 @@ public class MainActivity extends BaseFragmentActivity {
         mDrawerToggle.syncState();//初始化状态
         mainBinding.drawerLayout.setDrawerListener(mDrawerToggle);
 
+        int width = 4 * ScreenUtils.getScreenWidth(this) / 5;
+
         // 设置侧滑菜单 ListView_Slide
-        ViewGroup.LayoutParams params = mainBinding.listSlide.getLayoutParams();
-        params.width = 4 * ScreenUtils.getScreenWidth(this) / 5;
-        mainBinding.listSlide.setLayoutParams(params);
+        ViewGroup.LayoutParams params = mainBinding.scrollSlide.getLayoutParams();
+        params.width = width;
+        mainBinding.scrollSlide.setLayoutParams(params);
+
+        // 设置背景图高度
+        ViewGroup.LayoutParams imgBgParams = mainBinding.imgPersonBg.getLayoutParams();
+        imgBgParams.height = (int) (0.618 * width);
+        mainBinding.imgPersonBg.setLayoutParams(imgBgParams);
     }
 
     /**
      * 标题栏左侧按钮点击事件
      */
     public ReplyCommand onLeftCommand = new ReplyCommand(() -> mainBinding.drawerLayout.openDrawer(Gravity.START));
+
+    /**
+     * 头像点击事件
+     */
+    public ReplyCommand onAvatarCommand = new ReplyCommand(() -> {
+        ToastUtil.showShortToast(mContext, "头像点击事件");
+    });
+
+    /**
+     * 背景图像点击事件
+     */
+    public ReplyCommand onPersonBgCommand = new ReplyCommand(() -> {
+        ToastUtil.showShortToast(mContext, "背景图像点击事件");
+    });
 
     @Override
     public void onBackPressed() {
---
+++ app/src/main/java/com/penoder/dailynews/ui/bookshelf/ShelfFragment.java
@@ -0,0 +1,22 @@
+package com.penoder.dailynews.ui.bookshelf;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.penoder.dailynews.ui.basic.BaseFragment;
+
+/**
+ * @author Penoder
+ * @date 18-4-25.
+ */
+public class ShelfFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
